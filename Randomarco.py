7119595189752392d626f261ea9cba013aba0e6b
marcodene <65088375+marcodene@users.noreply.github.com>
2020-10-25 17:54:28 +0100

    completed comments

--- Randomarco.py
+++ Randomarco.py
@@ -1,9 +1,10 @@
 import random
 import tkinter as tk
 
+#list of all the students
 students = ['Abbotto', 'Anglieri', 'Briola', 'Budeanu', 'Cattai', 'Celant', 'De Negri', 'Du', 'Dubini', 'Gandolfi', 'Grimaldi', 'Licciardi', 'Maggiore', 'Mazzotti', 'Micara', 'Mihindukulasuria', 'Monteduro', 'Orlando', 'Piana', 'Ruiz', 'Stella', 'Tettamanti', 'Zola']
 
-
+#Function to pick a random student
 def random_picker():
     # pick a random number from 0 to the length of the student list
     num = random.randint(0, len(students)-1)
@@ -14,11 +15,15 @@ def random_picker():
 
 window = tk.Tk()
 
-
+# btn widget => when clicked it gets a random student
 btn_picker = tk.Button(text="Random", command=random_picker)
+
+# lbl to display the random student picked
 lbl_result = tk.Label()
 
+# Display the btn and the lbl
 btn_picker.pack()
 lbl_result.pack()
 
+#Display the window
 window.mainloop()
