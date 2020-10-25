a8b5570f186207f2454b8059d33f8f73eb19a3e7
marcodene <65088375+marcodene@users.noreply.github.com>
2020-10-25 19:36:03 +0100

    added the title and if the last group of students left

--- Randomarco.py
+++ Randomarco.py
@@ -7,6 +7,7 @@ students = ['Abbotto', 'Anglieri', 'Briola', 'Budeanu', 'Cattai', 'Celant', 'De 
 #length of the students list
 length = len(students)
 
+proced = False
 
 prev_txt = ''
 
@@ -15,6 +16,7 @@ def random_picker():
     global length
     global input_field
     global prev_txt
+    global proced
 
 
     #Clean the lbl_result
@@ -33,34 +35,49 @@ def random_picker():
         elif n_to_repeat.isdigit() == False:
             lbl_result["text"] = "You haven't entered a number.\nPlease enter a number."
 
-        # if the string of the input is a too high than ask to enter a minor number
-        elif int(n_to_repeat) > length - 1:
+        # if the string of the input is a too high than ask to enter a minor number. This runs only if the user hasn't already insert an available number
+        elif int(n_to_repeat) > length - 1 and proced == False:
             lbl_result["text"] = "There aren't enough students in the class.\nPlease enter a minor number."
            
 
         else:
+            proced = True
 
-            for i in range(int(n_to_repeat)):
-                # pick a random number from 0 to the length of the student list
-                num = random.randint(0, length-1)
+            #This runs if the user has already insert an available number, but it is higher than the students left
+            if int(n_to_repeat) > length - 1:
+                for i in range (length):
+                    if prev_txt == '':
+                        txt = students[i]
+                    else:
+                        txt = f'{prev_txt}, {students[i]}'
+                    print(txt)
 
-                print('num: ' + str(num))
-                
-                # set the lbl_result text to the random student picked
-                if prev_txt == '':
-                    lbl_result["text"] = str(students[num])
-                else:
-                    lbl_result["text"] = f'{prev_txt}, {str(students[num])}'
+                    prev_txt = txt
 
-                prev_txt = lbl_result["text"]
-                
+                lbl_result["text"] = f"There is not another group of {n_to_repeat} people.\nThere is a group of {length} people\n{txt}"
 
-                #remove the index of the student picked from the list
-                students.pop(num)
-                #refresh the students length
-                length = len(students)
-                
-                print('students left: ' + str(length))
+            else:
+                for i in range(int(n_to_repeat)):
+                    # pick a random number from 0 to the length of the student list
+                    num = random.randint(0, length-1)
+
+                    print('num: ' + str(num))
+                    
+                    # set the lbl_result text to the random student picked
+                    if prev_txt == '':
+                        lbl_result["text"] = str(students[num])
+                    else:
+                        lbl_result["text"] = f'{prev_txt}, {str(students[num])}'
+
+                    prev_txt = lbl_result["text"]
+                    
+
+                    #remove the index of the student picked from the list
+                    students.pop(num)
+                    #refresh the students length
+                    length = len(students)
+                    
+                    print('students left: ' + str(length))
 
             lbl_stud_left["text"] = f'There are {length} students left.'
         
@@ -72,6 +89,8 @@ def random_picker():
 
 
 window = tk.Tk()
+window.title('RANDOMARCO')
+
 #lbl ask how many student must be picked
 lbl_num_to_pick = tk.Label(text = 'How many students do you wanna pick?')
 
