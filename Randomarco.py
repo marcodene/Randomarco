98199c144db66c49d1345d3bc130f3474d15bcce
marcodene <65088375+marcodene@users.noreply.github.com>
2020-10-25 18:32:28 +0100

    input field. blank or non digit
    added an input field and check if the txt of the input is blank or a non digit txt

--- Randomarco.py
+++ Randomarco.py
@@ -10,22 +10,35 @@ length = len(students)
 #Function to pick a random student and remove it from the student list
 def random_picker():
     global length
+    global input_field
 
     if length != 0 :
-        # pick a random number from 0 to the length of the student list
-        num = random.randint(0, length-1)
+        #it gets from the input_field how many student to pick up
+        n_to_repeat = input_field.get()
 
-        print('num: ' + str(num))
+        # if the string of the input is blank than ask to enter a number
+        if n_to_repeat == '':
+            lbl_result["text"] = "You have entered nothing.\nPlease enter a number."
 
-        # set the lbl_result text to the random student picked
-        lbl_result["text"] = str(students[num])
+        # if the string of the input is a non-digit than ask to enter a number
+        elif n_to_repeat.isdigit() == False:
+            lbl_result["text"] = "You haven't entered a number.\nPlease enter a number."
 
-        #remove the index of the student picked from the list
-        students.pop(num)
-        #refresh the students length
-        length = len(students)
-        
-        print('students left: ' + str(length))
+        else:
+            # pick a random number from 0 to the length of the student list
+            num = random.randint(0, length-1)
+
+            print('num: ' + str(num))
+
+            # set the lbl_result text to the random student picked
+            lbl_result["text"] = str(students[num])
+
+            #remove the index of the student picked from the list
+            students.pop(num)
+            #refresh the students length
+            length = len(students)
+            
+            print('students left: ' + str(length))
         
     else:
         #if the students are finished then display that there are no more students available
@@ -35,14 +48,21 @@ def random_picker():
 
 
 window = tk.Tk()
+#lbl ask how many student must be picked
+lbl_num_to_pick = tk.Label(text = 'How many students do you wanna pick?')
+
+#input field where you can set how many students you want to be picked
+input_field = tk.Entry()
 
 # btn widget => when clicked it gets a random student
-btn_picker = tk.Button(text="Random", command=random_picker)
+btn_picker = tk.Button(text="Pick", command=random_picker)
 
 # lbl to display the random student picked
 lbl_result = tk.Label()
 
 # Display the btn and the lbl
+lbl_num_to_pick.pack()
+input_field.pack()
 btn_picker.pack()
 lbl_result.pack()
 
