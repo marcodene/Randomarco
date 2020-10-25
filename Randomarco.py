1228c75dae73bdb335cb99ff98254fa04d1ece3b
marcodene <65088375+marcodene@users.noreply.github.com>
2020-10-25 18:09:40 +0100

    Remove the students picked from the list

--- Randomarco.py
+++ Randomarco.py
@@ -4,12 +4,28 @@ import tkinter as tk
 #list of all the students
 students = ['Abbotto', 'Anglieri', 'Briola', 'Budeanu', 'Cattai', 'Celant', 'De Negri', 'Du', 'Dubini', 'Gandolfi', 'Grimaldi', 'Licciardi', 'Maggiore', 'Mazzotti', 'Micara', 'Mihindukulasuria', 'Monteduro', 'Orlando', 'Piana', 'Ruiz', 'Stella', 'Tettamanti', 'Zola']
 
-#Function to pick a random student
+#length of the students list
+length = len(students)
+
+#Function to pick a random student and remove it from the student list
 def random_picker():
-    # pick a random number from 0 to the length of the student list
-    num = random.randint(0, len(students)-1)
-    # set the lbl_result text to the random student picked
-    lbl_result["text"] = str(students[num])
+    global length
+    
+    if length != 0 :
+        # pick a random number from 0 to the length of the student list
+        num = random.randint(0, length-1)
+        print('num: ' + str(num))
+        # set the lbl_result text to the random student picked
+        lbl_result["text"] = str(students[num])
+
+        #remove the index of the student picked from the list
+        students.pop(num)
+        #refresh the students length
+        length = len(students)
+        print('students left: ' + str(length))
+    else:
+        lbl_result["text"] = 'There are no more students'
+    
 
 
 
